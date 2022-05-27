173a2e9a015066e210d5be05ec976930107925a2
Padilha701 <padilhakawashakil@gmail.com>
2022-05-27 00:17:50 -0300

    Maps API

--- js/app.js
+++ js/app.js
@@ -1,13 +1,40 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+window.onload = function (){
+    var map;
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(map);
+    function initialize(){
+        var mapProp = {
+            center: new google.maps.LatLng(-23.503164654,-47.608497566),
+            scrollwheel: false,
+            zoom:12,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+        }
+        map = new google.maps.Map(document.getElementById("map"), mapProp);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+    }
+    
+    function addMarker(lat,long,icon,content){
+        var latLng = {'lat':lat,'lng':long};
 
+        var marker = new google.maps.Marker({
+            position:latLng,
+            map: map,
+            icon: icon
+        });
+
+        var infoWindow = new google.maps.InfoWindow({
+            content: content,
+            maxWidth: 200,
+            pixelOffset: new google.maps.Size(0,20)
+        });
+
+        google.maps.event.addListener(marker,'click', function(){
+            infoWindow.open(map,marker);
+        });
+        
+    }
+
+    initialize();
+
+    var conteudo = '<p style="Color:black;font-size:13px;padding:10px 0;">Meu Endereço</p>';
+    addMarker(-23.504390,-47.614330,'',conteudo);
+}
